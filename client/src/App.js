7a9b12eb6799f3e3fa77a090d788fc05b684902f
decimal21 <sameer.narendran@gmail.com>
2022-01-03 21:57:50 -0500

    added screens

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,10 @@
 import React from 'react';
-import NavigationBar from './components/NavigationBar';
-import Header from './components/Header';
-import Reviews from './components/Reviews';
-import { useState } from 'react';
+import Home from './screens/Home.js';
 
 const App = () => {
-	const [reviews, setReviews] = useState([
-		{
-			id: 1,
-			name: 'Burger',
-			rest: 'Burger King',
-			rating: 4.5,
-			lastDate: '2021-10-10',
-            private: true,
-		},
-		{
-			id: 2,
-			name: 'Fries',
-			rest: "McDonald's",
-			rating: 4,
-			lastDate: '2021-12-04',
-            private: false,
-		},
-		{
-			id: 3,
-			name: 'McFlurry',
-			rest: "McDonald's",
-			rating: 5,
-			lastDate: '2021-12-04',
-            private: true,
-		},
-		{
-			id: 4,
-			name: 'Burger',
-			rest: "Wendy's",
-			rating: 2,
-			lastDate: '2021-05-19',
-            private: true,
-		},
-		{
-			id: 5,
-			name: 'Fries',
-			rest: "Wendy's",
-			rating: 3,
-			lastDate: '2021-05-19',
-            private: false,
-		},
-	]);
-
-	//delete review
-	const deleteReview = (id) => {
-		console.log('deleting ', id);
-		setReviews(reviews.filter((review) => review.id !== id));
-	};
-
 	return (
 		<div>
-			<Header title='Food Review' bgColor='indianred' txtColor='white' />
-			<NavigationBar
-				navs={[
-					{ link: '/my-reviews', text: 'My Reviews' },
-					{ link: '/explore-reviews', text: 'Explore Reviews' },
-				]}
-			/>
-			{/* {reviews.length === 0 ? (
-				'You have no reviews'
-			) : (
-				<Reviews reviews={reviews} onDelete={deleteReview} />
-			)} */}
-			<Reviews reviews={reviews} onDelete={deleteReview} />
+			<Home />
 		</div>
 	);
 };
